53feb554e8a4c5cc1e69045b38fb599f325d4725
M Iqbal Tawakal <mit.iqi@gmail.com>
2024-08-15 15:19:35 +0700

    Add month and calendar plot to lesley showcase

--- pages/lesley.py
+++ pages/lesley.py
@@ -4,6 +4,8 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+import calendar
+
 st.set_page_config(layout='wide')
 st.title('Lesley')
 
@@ -20,3 +22,15 @@ with heatmap_container:
     height=270
     chart_heatmap = lesley.cal_heatmap(dates, values)
     st.altair_chart(chart_heatmap, use_container_width=False)
+
+month_container = st.container()
+with month_container:
+    month = st.selectbox('Select month', calendar.month_name[1:])
+    idx = list(calendar.month_name).index(month)
+    month_plot = lesley.month_plot(dates, values, month=idx)
+    st.altair_chart(month_plot)
+
+year_container = st.container()
+with year_container:
+    year_plot = lesley.calendar_plot(dates, values)
+    st.altair_chart(year_plot)
