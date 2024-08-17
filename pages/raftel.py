d27db05e6a9b617ec63b0f36d554323697b1843b
M Iqbal Tawakal <mit.iqi@gmail.com>
2024-08-17 22:09:55 +0700

    update raftel page

--- pages/raftel.py
+++ pages/raftel.py
@@ -1,23 +1,27 @@
+import os
+import raftel as rt
 import streamlit as st
 
-import raftel as rt
-
+st.image('pages/raftel_banner.png', width=1000)
 st.title('Raftel')
 
-option = st.radio('Select Input Type', ['Coordinate', 'S2ID'])
+cols = st.columns([0.2, 0.8])
 
-if option == 'Coordinate':
+with cols[0]:
+    with st.form('s2_form'):
 
-    lat = st.text_input('Insert Latitude', '-6.1944')
-    lon = st.text_input('Insert Longitude', '106.8229')
+        lat = st.text_input('Insert Latitude', '-6.1944')
+        lon = st.text_input('Insert Longitude', '106.8229')
+        s2id_level = st.number_input('Insert S2ID level', value=9)
 
-    s2id_level = st.number_input('Insert S2ID level', value=7)
+        submit = st.form_submit_button('submit')
 
-import os
 
 api_key = os.environ['STADIA_API_KEY']
 
-s2id = rt.get_s2id(float(lat), float(lon), s2id_level)
-peta = rt.plot_s2id([s2id], api_key=api_key)
+with cols[1]:
+    if submit:
+        s2id = rt.get_s2id(float(lat), float(lon), s2id_level)
+        peta = rt.plot_s2id([s2id], api_key=api_key)
 
-st.image(peta)
+        st.image(peta)
