6ddc3011bdb5f7957631b994220fe78fa5443213
M Iqbal Tawakal <mit.iqi@gmail.com>
2024-08-18 15:26:29 +0700

    add option to plot line in the monochromap

--- pages/monochromap.py
+++ pages/monochromap.py
@@ -4,12 +4,21 @@ import monochromap as mono
 
 st.title('Monochromap')
 
+shape = st.selectbox('Select type of object', ['Point', 'Line', 'Image'])
+
 with st.form('canvas'):
 
-    st.selectbox('Select type of object', ['Point'])
-
-    lat = st.text_input('Input Latitude')
-    lon = st.text_input('Input Longitude')
+    if shape == 'Point':
+        lat = st.number_input('Input Latitude', value=-6.1944)
+        lon = st.number_input('Input Longitude', value=106.8229)
+    elif shape == 'Line':
+        cols = st.columns(2)
+        with cols[0]:
+            lat1 = st.number_input('Input Lat Point 1', value=-6.0686695, min_value=-180., max_value=180.)
+            lon1 = st.number_input('Input Lon Point 1', value=105.8847784, min_value=-180., max_value=180.)
+        with cols[1]:
+            lat2 = st.number_input('Input Lat Point 2', value=-7.693067, min_value=-180., max_value=180.)
+            lon2 = st.number_input('Input Lon Point 2', value=113.9266419, min_value=-180., max_value=180.)
 
     submit = st.form_submit_button('Add to map')
     if submit:
@@ -17,8 +26,11 @@ with st.form('canvas'):
         if 'peta' not in st.session_state:
             st.session_state['peta'] = mono.MonochroMap(api_key=os.environ['STADIA_API_KEY'])
 
-        p = mono.Point((float(lon), float(lat)), '#fb294344', 5)
-        st.session_state['peta'].add_feature(p)
+        if shape == 'Point':
+            obj = mono.Point((float(lon), float(lat)), '#fb294344', 5)
+        elif shape == 'Line':
+            obj = mono.Line(coords=[(lon1, lat1), (lon2, lat2)], color='#fb294344', width=5)
+        st.session_state['peta'].add_feature(obj)
 
 if 'peta' in st.session_state:
     img = st.session_state['peta'].render()
