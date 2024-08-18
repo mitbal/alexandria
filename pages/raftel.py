7427ae72eecde3d9a8fa726835ce007d5b953dcc
M Iqbal Tawakal <mit.iqi@gmail.com>
2024-08-18 14:59:32 +0700

    added more parameter to plot s2id

--- pages/raftel.py
+++ pages/raftel.py
@@ -2,17 +2,21 @@ import os
 import raftel as rt
 import streamlit as st
 
+st.set_page_config(layout='wide')
+
 st.image('pages/raftel_banner.png', width=1000)
 st.title('Raftel')
 
-cols = st.columns([0.2, 0.8])
+cols = st.columns([0.25, 0.75])
 
 with cols[0]:
     with st.form('s2_form'):
 
-        lat = st.text_input('Insert Latitude', '-6.1944')
-        lon = st.text_input('Insert Longitude', '106.8229')
-        s2id_level = st.number_input('Insert S2ID level', value=9)
+        lat = st.number_input('Insert Latitude', value=-6.1944, min_value=-180., max_value=180.)
+        lon = st.number_input('Insert Longitude', value=106.8229, min_value=-180., max_value=180.)
+        s2id_level = st.number_input('Insert S2ID level', value=9, min_value=0, max_value=30)
+        alpha = st.number_input('Select Rectangle Transparency', value=0.5, min_value=0., max_value=1.0)
+        color = st.color_picker('Select Rectangle Color', value='#00ff00')
 
         submit = st.form_submit_button('submit')
 
@@ -22,6 +26,7 @@ api_key = os.environ['STADIA_API_KEY']
 with cols[1]:
     if submit:
         s2id = rt.get_s2id(float(lat), float(lon), s2id_level)
-        peta = rt.plot_s2id([s2id], api_key=api_key)
+        peta = rt.plot_s2id([s2id], api_key=api_key, color=color, alpha=alpha)
 
+        st.write(f'S2ID {s2id}')
         st.image(peta)
