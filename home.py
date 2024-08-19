53280192497ee974550b6df282e24f4ab1f3f4f1
M Iqbal Tawakal <mit.iqi@gmail.com>
2024-08-19 13:49:16 +0700

    add heading

--- home.py
+++ home.py
@@ -18,10 +18,10 @@ Lesley is a library to show heatmap on calendar data. It is heavily inspired by 
 ### 2. Floryn
 Floryn is a library to plot text filled in percentage, instead of full. This can be used to highlight the difference or ratio between several values or metrics.
 
-3. Raftel
+### 3. Raftel
 Raftel is a library to plot s2id into the map. S2ID is a way to perform geospatial analysis by dividing the map into quad tree. More about S2ID and its difference compare to other algorithm such as geohash or H3 can be learned further in this article.
 
-4. Monochromap
+### 4. Monochromap
 Monochromap is a library to plot on top of black and white map, or what we in the industry called Stamen Toner tile style. The black and white, instead of for example default tile style provided by google maps or open street maps, make it easier to highlight certain geographical features like places of point of interest.
 
 """
