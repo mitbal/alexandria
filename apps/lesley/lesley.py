b61a1b591afca3c67c73cdace0207865ec03ff2b
M Iqbal Tawakal <mit.iqi@gmail.com>
2024-08-20 15:20:30 +0700

    fix layout issue in lesley

--- apps/lesley/lesley.py
+++ apps/lesley/lesley.py
@@ -6,31 +6,45 @@ import pandas as pd
 
 import calendar
 
-st.set_page_config(layout='wide')
 st.title('Lesley')
 
-use_random = st.radio('Select one', ['True', 'False'])
+cmaps = ['viridis', 'plasma', 'inferno', 'magma', 'cividis', 'YlGn']
 
-if use_random:
+use_random = st.radio('Use Random Data?', ['True', 'False'])
+if use_random == 'True':
     dates = pd.date_range(start='2024-01-01', end='2024-12-31')
     values = np.random.randint(0, 10, size=len(dates))
 else:
-    st.stop()
+    st.write('Provide csv files with at least 2 columns, dates and values')
+    file_csv = st.file_uploader('Select File')
 
-heatmap_container = st.container()
+    if file_csv:
+        df = pd.read_csv(file_csv)
+        dates = df['dates'].astype('datetime64[ns]')
+        values = df['values'].astype('float')
+    else:
+        st.stop()
+
+cmap = st.selectbox('Select Color Maps', cmaps, index=5)
+
+st.write('Calendar Heatmap function')
+heatmap_container = st.container(height=310)
 with heatmap_container:
     height=270
-    chart_heatmap = lesley.cal_heatmap(dates, values)
-    st.altair_chart(chart_heatmap, use_container_width=False)
+    chart_heatmap = lesley.cal_heatmap(dates, values, height=height, cmap=cmap)
+    st.altair_chart(chart_heatmap, use_container_width=True)
 
-month_container = st.container()
+st.write('Individual Month Plot')
+month_container = st.container(height=400)
 with month_container:
     month = st.selectbox('Select month', calendar.month_name[1:])
     idx = list(calendar.month_name).index(month)
-    month_plot = lesley.month_plot(dates, values, month=idx)
-    st.altair_chart(month_plot)
+    month_plot = lesley.month_plot(dates, values, month=idx, height=250, cmap=cmap)
+    st.altair_chart(month_plot, use_container_width=False)
 
-year_container = st.container()
+st.write('Entire Year Plot')
+year_container = st.container(height=850)
 with year_container:
-    year_plot = lesley.calendar_plot(dates, values)
+    nrows = st.selectbox('Insert Number of Rows', [1, 2, 3, 4, 6, 12], index=2)
+    year_plot = lesley.calendar_plot(dates, values, nrows=nrows, cmap=cmap)
     st.altair_chart(year_plot)
