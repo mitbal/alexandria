b25731ab7661fc80ca757d072e8055d61a200fef
M Iqbal Tawakal <mit.iqi@gmail.com>
2024-08-20 15:19:12 +0700

    moved lesley and floryn to separate directory

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
 page_home = st.Page('home.py', title='Home', icon='📚')
-page_lesley = st.Page('apps/lesley.py', title='Lesley', icon='📅')
-page_floryn = st.Page('apps/floryn.py', title='Floryn', icon='🌼')
-page_raftel = st.Page('apps/raftel.py', title='Raftel', icon='🟩')
+page_lesley = st.Page('apps/lesley/lesley.py', title='Lesley', icon='📅')
+page_floryn = st.Page('apps/floryn/floryn.py', title='Floryn', icon='🌼')
+page_raftel = st.Page('apps/raftel/raftel.py', title='Raftel', icon='🟩')
 page_monoch = st.Page('apps/monochromap/monochromap.py', title='Monochromap', icon='🗺️')
 pages = st.navigation(
     {
