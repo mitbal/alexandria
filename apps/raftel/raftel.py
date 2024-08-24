4cf9b33b8c737ab727492f5c15a00161be1ba1d8
M Iqbal Tawakal <mit.iqi@gmail.com>
2024-08-24 14:30:01 +0700

    add placeholder and make the map persistent in raftel apps

--- apps/raftel/raftel.py
+++ apps/raftel/raftel.py
@@ -6,9 +6,14 @@ st.set_page_config(layout='wide')
 
 st.image('apps/raftel/raftel_banner.png', width=1000)
 st.title('Raftel')
+api_key = os.environ['STADIA_API_KEY']
+
+reset_button = st.button(label='Reset Map')
+if reset_button:
+    if 'peta' in st.session_state:
+        del st.session_state['peta']
 
 cols = st.columns([0.25, 0.75])
-
 with cols[0]:
     with st.form('s2_form'):
 
@@ -19,14 +24,18 @@ with cols[0]:
         color = st.color_picker('Select Rectangle Color', value='#00ff00')
 
         submit = st.form_submit_button('submit')
+        if submit:
+            s2id = rt.get_s2id(float(lat), float(lon), s2id_level)
+            if 'peta' not in st.session_state:
+                st.session_state['peta'] = rt.plot_s2id([s2id], api_key=api_key, color=color, alpha=alpha, auto_render=False)
+            else:
+                st.session_state['peta'] = rt.plot_s2id([s2id], api_key=api_key, color=color, alpha=alpha, auto_render=False, m=st.session_state['peta'])
 
+            st.write(f'adding S2ID {s2id}')
 
-api_key = os.environ['STADIA_API_KEY']
 
 with cols[1]:
-    if submit:
-        s2id = rt.get_s2id(float(lat), float(lon), s2id_level)
-        peta = rt.plot_s2id([s2id], api_key=api_key, color=color, alpha=alpha)
-
-        st.write(f'S2ID {s2id}')
-        st.image(peta)
+    if 'peta' in st.session_state:
+        st.image(st.session_state['peta'].render())
+    else:
+        st.image('apps/raftel/raftel_placeholder.png')
