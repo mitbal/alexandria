122a7ada7b9b4c327e46fb8b2aa0c6b32ac998aa
M Iqbal Tawakal <mit.iqi@gmail.com>
2026-03-31 17:02:22 +0700

    add panen dividen button link

--- main.py
+++ main.py
@@ -20,6 +20,20 @@ pages = st.navigation(
     }
 )
 
-st.sidebar.html('Other project<br/><a href=https://panendividen.com>Panen Dividen</a>')
+import base64
+
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def get_img_with_href(local_img_path, target_url):
+    img_format = 'svg+xml'
+    bin_str = get_base64_of_bin_file(local_img_path)
+    html_code = f'Visit Other Project:<br/><a href="{target_url}"><img src="data:image/{img_format};base64,{bin_str}" width="200"></a>'
+    return html_code
+
+image_html = get_img_with_href('panen_dividen.svg', 'https://panendividen.com')
+st.sidebar.html(image_html)
 
 pages.run()
