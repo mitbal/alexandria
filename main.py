bd702daecb8baff3bf4981a0398d69ffe2fdf0bb
M Iqbal Tawakal <mit.iqi@gmail.com>
2026-07-07 21:38:30 +0700

    add csv explorer and finplot pages, remove duplicate subreddit_stats.csv

--- main.py
+++ main.py
@@ -14,13 +14,15 @@ page_raftel = st.Page('apps/raftel/raftel.py', title='Raftel', icon='🟩')
 page_monoch = st.Page('apps/monochromap/monochromap.py', title='Monochromap', icon='🗺️')
 page_mbg = st.Page('apps/mbg/mbg.py', title='MBG Plot', icon='🍱')
 page_vortree = st.Page('apps/vortree/vortree.py', title='Voronoi Treemap', icon='🌀')
+# page_csv_explorer = st.Page('apps/csv_explorer/csv_explorer.py', title='CSV Explorer', icon='📊')
+page_finplot = st.Page('apps/finplot/finplot.py', title='FinPlot', icon='💹')
 page_reddit = st.Page('apps/reddit_stats/reddit_stats.py', title='Reddit Stats', icon='🤖')
 
 pages = st.navigation(
     {
         'Home': [page_home],
         'Libraries': [page_lesley, page_floryn, page_raftel, page_monoch, page_mbg, page_vortree],
-        'Utilities': [page_reddit],
+        'Utilities': [page_finplot, page_reddit],
     }
 )
 
